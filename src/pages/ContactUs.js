edfa126e55141ca3fd83a576c8ee545c2e288cd7
Cat <money1062@live.com>
2020-03-05 12:16:35 -0500

    probably last update

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -27,104 +27,104 @@ const ContactUs = () => {
 
   return (
     <>
-      <div id="contactUsPageContactInfo">
+      <div id="contactInfo">
         <h2>Contact Us</h2>
         <p>Ipsum Lorem R&D Office</p>
         <p><small>420 Park Avenue</small></p>
         <p><small>Miami, FL 331XX</small></p>
         <p><small>xxx-456-7890</small></p>
+        <h2>Email Addresses</h2>
         <p>General Information: info@ipsum.xyz</p>
         <p>Sales: sales@ipsum.xyz</p>
         <p>Customer Support: support@ipsum.xyz</p>
         <p>Webmaster: admin@ipsum.xyz</p>
       </div>
-      <div id="contactUsPageMessageForm">
+      <div id="messageForm">
         <h2>Send us a Message</h2>
         <form onSubmit={handleSubmit}>
-          <div class="inputLine">
-            {/* <label htmlFor='name'></label> */}
-            <input
-              type='name'
-              name='name'
-              value={name}
+          <div>
+            <div class="input">
+              {/* <label htmlFor='name'></label> */}
+              <input
+                type='name'
+                name='name'
+                value={name}
+                onChange={handleInput}
+                placeholder='Name'
+              />
+              {/* <label htmlFor='name'></label> */}
+              <input
+                type='company'
+                name='company'
+                value={company}
+                onChange={handleInput}
+                placeholder='Company'
+              />
+              {/* <label htmlFor='name'></label> */}
+              <input
+                type='Address'
+                name='Address'
+                value={address}
+                onChange={handleInput}
+                placeholder='Address'
+              />
+            </div>
+            <div class="input">
+              {/* <label htmlFor='name'></label> */}
+              <input
+                type='telephone'
+                name='telephone'
+                value={telephone}
+                onChange={handleInput}
+                placeholder='Telephone'
+              />
+              {/* <label htmlFor='name'></label> */}
+              <input
+                type='fax'
+                name='fax'
+                value={fax}
+                onChange={handleInput}
+                placeholder='FAX'
+              />
+              {/* <label htmlFor='name'></label> */}
+              <input
+                type='email'
+                name='email'
+                value={email}
+                onChange={handleInput}
+                placeholder='Email'
+              />
+            </div>
+          </div>
+          <div id="areaInput">
+            <textarea
+              types='comments'
+              name='comments'
+              value={comments}
               onChange={handleInput}
-              placeholder='Name'
+              placeholder=':)...'
+              rows='5'
+              cols='63'
             />
           </div>
-          <div class="inputLine">
-            {/* <label htmlFor='name'></label> */}
-            <input
-              type='company'
-              name='company'
-              value={company}
+          <div>
+            <label style={{marginLeft: "0.3em"}} id="selectLabel" for='category'>Category: </label>
+            <select
+              id="selectInput"
+              type='category'
+              name='category'
+              value={category}
               onChange={handleInput}
-              placeholder='Company'
-            />
-          </div>
-          <div class="inputLine">
-            {/* <label htmlFor='name'></label> */}
-            <input
-              type='Address'
-              name='Address'
-              value={address}
-              onChange={handleInput}
-              placeholder='Address'
-            />
-          </div>
-          <div class="inputLine">
-            {/* <label htmlFor='name'></label> */}
-            <input
-              type='telephone'
-              name='telephone'
-              value={telephone}
-              onChange={handleInput}
-              placeholder='Telephone'
-            />
-          </div>
-          <div class="inputLine">
-            {/* <label htmlFor='name'></label> */}
-            <input
-              type='fax'
-              name='fax'
-              value={fax}
-              onChange={handleInput}
-              placeholder='FAX'
-            />
-          </div>
-          <div class="inputLine">
-            {/* <label htmlFor='name'></label> */}
-            <input
-              type='email'
-              name='email'
-              value={email}
-              onChange={handleInput}
-              placeholder='Email'
-            />
-          </div>
-          <textarea
-            types='comments'
-            name='comments'
-            value={comments}
-            onChange={handleInput}
-            placeholder=':)...'
-            rows='4'
-            cols='50'
-          />
-          <label for='category'>Category: </label>
-          <select
-            type='category'
-            name='category'
-            value={category}
-            onChange={handleInput}
-          >
-            <option value="Website">Website</option>
-            <option value="Company">Company</option>
-            <option value="Product">Products</option>
-            <option value="Services">Services</option>
-          </select>
-          <button type='submit'>
-            Submit
+            >
+              <option value="Website">Website</option>
+              <option value="Company">Company</option>
+              <option value="Product">Products</option>
+              <option value="Services">Services</option>
+            </select>
+            <button id="formSubmitBtn" type='submit'>
+              Submit
             </button>
+          </div>
         </form>
       </div>
     </>
